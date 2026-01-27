edb334f537e75d1c91a95c7733a987764111e6d4
Samuel Murray <sammurray360@gmail.com>
2026-01-27 10:27:41 -0600

    init commit

---
+++ public/controller/firebase_core.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+   apiKey: "AIzaSyBuGAvghfT50MOChYdAVpKJR2ZkiB84-ak",
+   authDomain: "smurray18-cmsc4373.firebaseapp.com",
+   projectId: "smurray18-cmsc4373",
+   storageBucket: "smurray18-cmsc4373.firebasestorage.app",
+   messagingSenderId: "744375961291",
+   appId: "1:744375961291:web:028eeb507b1104f06bbd4e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
