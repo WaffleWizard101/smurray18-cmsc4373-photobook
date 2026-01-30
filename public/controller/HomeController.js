62534d0724d0593ba109dfe4ca4070aafe1df80f
Samuel Murray <sammurray360@gmail.com>
2026-01-30 11:59:35 -0600

    Working MVC implementation of home view table

--- public/controller/HomeController.js
+++ public/controller/HomeController.js
@@ -7,9 +7,17 @@ export class HomeController {
 
    constructor() {
       this.model = new HomeModel();
+      this.onClickGenerateDataButton = this.onClickGenerateDataButton.bind(this);
    }
 
    setView(view) {
       this.view = view;
+   }
+
+   onClickGenerateDataButton() {
+      //Random number between 1 and 100
+      const randomNumber = Math.floor(Math.random() * 100) + 1;
+      this.model.addNumber(randomNumber);
+      this.view.render();
    }
 }
--- public/model/HomeModel.js
+++ public/model/HomeModel.js
@@ -1,3 +1,16 @@
 export class HomeModel {
-   
+   numberList = [];
+
+   getNumberList() {
+      return this.numberList;
+   }
+
+   addNumber(number) {
+      this.numberList.push(number);
+   }
+
+   reset() {
+      this.numberList = [];
+   }
+
 }
