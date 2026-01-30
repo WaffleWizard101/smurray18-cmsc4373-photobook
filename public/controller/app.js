1a6baccb0b59bec06d6583a50f50449fccca474d
Samuel Murray <sammurray360@gmail.com>
2026-01-30 17:08:37 -0600

    Loading spinner implemented

--- public/controller/app.js
+++ public/controller/app.js
@@ -4,6 +4,7 @@ import { HomeController } from "./HomeController.js";
 import { ProfileController } from "./ProfileController.js";
 import { Router } from "./Router.js";
 import { createAccount, loginFirebase, logoutFirebase } from "./firebase_auth.js"
+import { startSpinner, stopSpinner } from "../view/util.js";
 
 document.getElementById('appHeader').textContent = 'Cloud Web Template';
 document.title = 'App Template';
@@ -37,10 +38,13 @@ document.forms.loginForm.onsubmit = async function(e) {
    e.preventDefault(); // prevent form submission from reloading page
    const email = e.target.email.value
    const password = e.target.password.value;
+   startSpinner();
    try {
       await loginFirebase(email, password);
+      stopSpinner();
       console.log('User logged in', email);
    } catch (e) {
+      stopSpinner();
       console.error('Error logging in:', e);
       const errorCode = e.code;
       const errorMessage = e.message;
@@ -50,11 +54,14 @@ document.forms.loginForm.onsubmit = async function(e) {
 
 //logout button
 document.getElementById('logoutButton').onclick = async function(e) {
+   startSpinner();
    try {
       await logoutFirebase();
+      stopSpinner();
       console.log('User logged out.');
    } catch (e) {
       console.error('Error logging out:', e);
+      stopSpinner();
       const errorCode = e.code;
       const errorMessage = e.message;
       alert('Sign out failed: ' + errorCode.code + ' ' + error.message);
@@ -72,11 +79,15 @@ document.forms.createAccountForm.onsubmit = async function(e) {
       alert('Emails do not match!');
       return;
    }
+
+   startSpinner();
    try {
       await createAccount(email, password);
+      stopSpinner();
       document.getElementById('createAccountDiv').classList.replace('d-block', 'd-none');
    } catch (e) {
       console.error('Error creating account', e);
+      stopSpinner();
       const errorCode = e.code;
       const errorMessage = e.message;
       alert('Account creation failed: ' + errorCode + ' ' + errorMessage);
